9812c284e4014aeb3612efcd8f61547bfcddf2b9
shasta28 <jessahafer-zdral@Jessas-MacBook-Pro.local>
2016-05-07 11:39:42 -0400

    refactor movement to trail body

--- js/snake.js
+++ js/snake.js
@@ -2,9 +2,9 @@ var grid = {
 	width: 40,
 	height: 40,
 	draw: function() {
-		for (y=0; y < grid.height; y++) {
+		for (y=0; y < this.height; y++) {
 			var row = $('<div class="row"></div>').appendTo($("#board"));
-			for (x=0; x < grid.width; x++) {
+			for (x=0; x < this.width; x++) {
 				$(row).append('<div class="cell" data-coord="'+x+'-'+y+'"></div>');
 			}
 		}
@@ -13,7 +13,7 @@ var grid = {
 
 var snake = {
 	facing: "e",
-	body: [[19,19]],
+	body: [[19,19],[18,19],[17,19]],
 	speed: 200,
 	render: function() {
 		for (i=0; i<this.body.length; i++) {
@@ -24,38 +24,36 @@ var snake = {
 		}
 	},
 	move: function() {
-		console.log(this);
-		$('.snake').remove();
+		var head = this.body[0];
+		var vacated = [head[0],head[1]]
 		switch(this.facing) {
 			case "n":
-				if (this.body[0][1] > 0) {
-					for (i=0; i<this.body.length; i++) {
-						this.body[i][1]--;
-					};
+				if (head[1] > 0) {
+					head[1]--;
 				}
 				break;
 			case "s":
-				if (this.body[0][1] < grid.height-1) {
-					for (i=0; i<this.body.length; i++) {
-						this.body[i][1]++;
-					};
+				if (head[1] < grid.height-1) {
+					head[1]++;
 				}
 				break;
 			case "e":
-				if (this.body[0][0] < grid.width-1) {
-					for (i=0; i<this.body.length; i++) {
-						this.body[i][0]++;
-					};
+				if (head[0] < grid.width-1) {
+					head[0]++;
 				}
 				break;
 			case "w":
-				if (this.body[0][0] > 0) {
-					for (i=0; i<this.body.length; i++) {
-						this.body[i][0]--;
-					};
+				if (head[0] > 0) {
+					head[0]--;
 				}
 				break;
 		}
+		for (i=1; i<this.body.length; i++) {
+			var current = [this.body[i][0],this.body[i][1]];
+			this.body[i] = vacated;
+			vacated = current;
+		}
+		$('.snake').remove();
 		this.render();
 	}
 };
